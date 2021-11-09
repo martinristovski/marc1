589fdc5d04f8858e18d9f62e0ed0fc30cf98d59b
angarc <angarc37@gmail.com>
2021-11-08 20:12:42 -0500

    added endpoint stubs

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
 import json
 import logging
 from datetime import datetime
+import uuid
+import secrets
 
 import utils.rest_utils as rest_utils
 
@@ -26,6 +28,48 @@ def health_check():
     rsp = Response(rsp_str, status=200, content_type="app/json")
     return rsp
 
+@app.route("/form/", methods=["POST"])
+def form_create():
+  data = request.get_json()
+  form_title = data["title"]
+  form_inputs = data["inputs"]
+  api_key = request.headers.get("api_key");
+
+  print("\n DATA: ", data, "\n API_KEY: ", api_key, "\n FORM_TITLE: ", form_title, "\n FORM_INPUTS: ", form_inputs)
+
+  return jsonify(data)
+
+@app.route("/form/", methods=["GET"])
+def form_get():
+  data = request.get_json()
+  form_id = data["form_id"]
+  api_key = request.headers.get("api_key");
+
+  # TODO: fetch form from database using form id
+
+  print("FORM_ID: ", form_id, "API_KEY", api_key)
+  return jsonify(data)
+
+@app.route("/developer/", methods=["GET"])
+def provision_api_key():
+  dev_uuid = uuid.uuid4()
+  api_key = secrets.token_urlsafe(32)
+
+  print("UUID: ", dev_uuid, "API_KEY: ", api_key)
+
+  return jsonify({"api_key": api_key, "uuid": dev_uuid})
+
+@app.route("/form-submission/", methods=["POST"])
+def submit_form_entry():
+  data = request.get_json()
+  form_id = data["form_id"]
+  submission_data = data["submission_data"]
+
+  # TODO: save submission data associated with form into mongo table
+
+  print("FORM_ID: ", form_id, "SUBMISSION_DATA", submission_data)
+
+  return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)

a313bc95034a25348a11e8642d5f538dbec56d10
angarc <angarc37@gmail.com>
2021-11-15 19:48:41 -0500

    beans now pass flake8

--- beans/form_input.py
+++ beans/form_input.py
@@ -1,5 +1,3 @@
-from re import template
-from flask import current_app, url_for
 from beans.form_input_elements import FormInputElements
 from database_services.RDBService import RDBDataTable
 from utils.validator import DataValidator
@@ -8,90 +6,106 @@ import middleware.context as md_context
 
 class FormInput:
 
-	def __init__(self, form_object):
-		"""
-		:param form_object: This contains the json object submitted by developer 
-		while creating a form template
-		"""
-		self.inputs = form_object.get("inputs", None)
-		self.endpoints = form_object.get("endpoints", None)
+    def __init__(self, form_object):
+        """
 
-	
-	def validate_form_values(self):
-		"""
-		This functions validates if fields(inputs, endpoints) are present or not.
-		It then checks that each input element provided by the end user has a valid
-		data type(str, int, float, boolean).
-		:return: Empty string if all the fields are valid. Else returns the appropriate reason
-		to send error_message to the end user
-		"""
-		reason = ""
-		valid_value_type = DataValidator.get_all_valid_types()
-		if self.inputs is None:
-			return f"Key input is missing"
+        :param form_object: This contains the
+          json object submitted by developer
+          while creating a form template
+        """
+        self.inputs = form_object.get("inputs", None)
+        self.endpoints = form_object.get("endpoints", None)
 
-		if self.endpoints is None:
-			return f"Key endpoints is missing"
+    def validate_form_values(self):
+        """
 
-		for input in self.inputs:
-			form_input_elements = FormInputElements(input)
-			if form_input_elements.field_name == None or form_input_elements.field_name == "":
-				reason = "One of the input field name is empty"
-				return reason
-			
-			input_instance = DataValidator.get_value_type(form_input_elements.field_type)
-			print(input_instance)
-			if input_instance is None:
-				reason = f"Invalid field_type = {form_input_elements.field_type} received. Valid types={valid_value_type}"
-				return reason
-		
-		return reason
+        This functions validates if fields(inputs, endpoints)
+        are present or not.
+        It then checks that each input element provided
+        by the end user has a valid
+        data type(str, int, float, boolean).
 
+        :return: Empty string if all the fields are valid.
+          Else returns the appropriate reason
+          to send error_message to the end user
+        """
+        reason = ""
+        valid_value_type = DataValidator.get_all_valid_types()
+        if self.inputs is None:
+            return "Key input is missing"
 
-	def delete_form_record(self, form_id, rdb_conn=md_context.get_rdb_info()):
-		"""
-		This function deletes a form_record from the form_info table. This function is used
-		when a developer decides to update his form template, i.e, add or remove fields
-		"""
-		form_info_db = RDBDataTable("form_info", connect_info=rdb_conn, key_columns=["uuid"])
-		template = {}
-		template['form_id'] = form_id
-		form_info_db.delete(template=template)
+        if self.endpoints is None:
+            return "Key endpoints is missing"
 
-	def process_form_creation(self, form_id, uuid, rdb_conn=md_context.get_rdb_info()):
-		"""
-		This function creates an entry in form_info table, form_column_mapper and
-		form_endpoint_mapper. Entries are created on the basis of form template 
-		provided by the developer
-		"""
-		form_info_db = RDBDataTable("form_info", connect_info=rdb_conn, key_columns=["uuid"])
+        for input in self.inputs:
+            form_input_elements = FormInputElements(input)
+            if (form_input_elements.field_name is None or
+               form_input_elements.field_name == ""):
 
-		form_data_info = {}
-		form_data_info['form_id'] = form_id
-		form_data_info['uuid'] = uuid
-		result_list = form_info_db.insert(form_data_info)
-		print(result_list)
-		form_column_db = RDBDataTable("form_column_mapper", connect_info=rdb_conn, key_columns=["form_id", "field_name"])
-		form_endpoint_db = RDBDataTable("form_endpoint_mapper", connect_info=rdb_conn)
+                reason = "One of the input field name is empty"
+                return reason
 
-		for input in self.inputs:
-			form_input_element = FormInputElements(input)
-			form_field_row = {}
-			form_field_row['form_id'] = form_id
-			form_field_row['field_name'] = form_input_element.field_name
-			form_field_row['field_type'] = form_input_element.field_type
-			form_field_row['expected_values'] = form_input_element.expected_value
-			form_column_record = form_column_db.insert(form_field_row)
-			print(form_column_record)
-		
-		for endpoint in self.endpoints:
-			form_endpoint_row = {}
-			form_endpoint_row['form_id'] = form_id
-			form_endpoint_row['accepted_endpoints'] = endpoint
-			form_endpoint_record = form_endpoint_db.insert(form_endpoint_row)
-			print(form_endpoint_record)
-			
-		
-		
+            input_instance = DataValidator.get_value_type(
+              form_input_elements.field_type)
+            print(input_instance)
+            if input_instance is None:
+                reason = f"Invalid field_type = {form_input_elements.field_type}\
+ received. Valid types={valid_value_type}"
+                return reason
 
-		
+        return reason
+
+    def delete_form_record(self, form_id, rdb_conn=md_context.get_rdb_info()):
+        """
+        This function deletes a form_record from the
+        form_info table. This function is used
+        when a developer decides to update his form
+        template, i.e, add or remove fields
+        """
+        form_info_db = RDBDataTable(
+          "form_info", connect_info=rdb_conn, key_columns=["uuid"])
+        template = {}
+        template['form_id'] = form_id
+        form_info_db.delete(template=template)
+
+    def process_form_creation(
+          self, form_id, uuid, rdb_conn=md_context.get_rdb_info()):
+        """
+        This function creates an entry in form_info table,
+        form_column_mapper and
+        form_endpoint_mapper. Entries are created
+        on the basis of form template
+        provided by the developer
+        """
+        form_info_db = RDBDataTable(
+          "form_info", connect_info=rdb_conn, key_columns=["uuid"])
+
+        form_data_info = {}
+        form_data_info['form_id'] = form_id
+        form_data_info['uuid'] = uuid
+        result_list = form_info_db.insert(form_data_info)
+        print(result_list)
+        form_column_db = RDBDataTable(
+          "form_column_mapper",
+          connect_info=rdb_conn,
+          key_columns=["form_id", "field_name"])
+        form_endpoint_db = RDBDataTable(
+          "form_endpoint_mapper", connect_info=rdb_conn)
+
+        for input in self.inputs:
+            form_input_element = FormInputElements(input)
+            form_field_row = {}
+            form_field_row['form_id'] = form_id
+            form_field_row['field_name'] = form_input_element.field_name
+            form_field_row['field_type'] = form_input_element.field_type
+            form_field_row['expected_values'] = \
+                form_input_element.expected_value
+            form_column_record = form_column_db.insert(form_field_row)
+            print(form_column_record)
+
+        for endpoint in self.endpoints:
+            form_endpoint_row = {}
+            form_endpoint_row['form_id'] = form_id
+            form_endpoint_row['accepted_endpoints'] = endpoint
+            form_endpoint_record = form_endpoint_db.insert(form_endpoint_row)
+            print(form_endpoint_record)
--- beans/submit_data_request.py
+++ beans/submit_data_request.py
@@ -6,123 +6,135 @@ from database_services.MongoDBTable import MongoDBTable
 import utils.rest_utils as RestUtils
 from utils.validator import DataValidator
 
+
 class SubmitFormDataRequest:
+    def __init__(self, submit_form_request):
+        """
+        :param form_object: This contains the json object submitted by user
+        when saving their data into our application.
+        """
+        self.form_id = submit_form_request.get("form_id", None)
+        self.submission_data = submit_form_request.get("submission_data", None)
+        self.table_name = "form_info"
 
-	def __init__(self, submit_form_request):
-		"""
-		:param form_object: This contains the json object submitted by user 
-		when saving their data into our application.
-		"""
-		self.form_id = submit_form_request.get("form_id", None)
-		self.submission_data = submit_form_request.get("submission_data", None)
-		self.table_name = "form_info"
+    def validate_form_request(self):
+        """
+        This function validates if the form_id and submission data
+        received in the request is present in our database or not.
+        :return: False if no record exists for the form_id provided else True
+        """
+        if (self.form_id is None) or (self.submission_data is None):
+            return False
 
+        template = {}
+        template['form_id'] = self.form_id
+        database_service = RDBDataTable(
+          "form_info",
+          connect_info=context.get_rdb_info(),
+          key_columns=["form_id"])
+        result = database_service.find_by_template(template)
+        current_app.logger.debug("The value of result is \
+          [" + str(result) + "]")
+        if result is None:
+            return False
 
-	def validate_form_request(self):
-		"""
-		This function validates if the form_id and submission data 
-		received in the request is present in our database or not.
-		:return: False if no record exists for the form_id provided else True
-		"""
-		if (self.form_id == None) or (self.submission_data == None):
-			return False
-		
-		template = {}
-		template['form_id'] = self.form_id
-		database_service = RDBDataTable("form_info", connect_info=context.get_rdb_info(), key_columns=["form_id"])
-		result = database_service.find_by_template(template)
-		current_app.logger.debug("The value of result is [" + str(result) + "]")
-		if result is None:
-			return False
-		
-		return True
+        return True
 
-	def parse_form_data(self):
-		"""
-		This function converts the request received to dict to be saved
-		in MongoDB.
-		:return: Returns the dictionary converted to be saved in MongoDB
-		"""
-		submission_data = self.submission_data
-		submission_dict = {}
-		for data in submission_data:
-			form_data = FormData(data)
-			submission_dict[form_data.field_name] = form_data.field_value
-		return submission_dict
+    def parse_form_data(self):
+        """
+        This function converts the request received to dict to be saved
+        in MongoDB.
+        :return: Returns the dictionary converted to be saved in MongoDB
+        """
+        submission_data = self.submission_data
+        submission_dict = {}
+        for data in submission_data:
+            form_data = FormData(data)
+            submission_dict[form_data.field_name] = form_data.field_value
+        return submission_dict
 
-	def validate_form_data(self, submitted_dict):
-		"""
-		This function validates if the submitted data is inline with
-		the template created by the developer. It checks the fields submitted
-		and the type of data received in the submitted value.
-		:return: Empty string if fields are valid, else returns the appropriate
-		error message.
-		"""
-		template = {}
-		template['form_id'] = self.form_id
-		submitted_keys = submitted_dict.keys()
-		expected_type_dict = {}
-		expected_value_dict = {}
-		database_service = RDBDataTable("form_column_mapper", connect_info=context.get_rdb_info(), key_columns=["form_id", "field_name"])
-		result_list = database_service.find_by_template(template, fields=['field_name', 'field_type','expected_values'])
-		if result_list is not None:
-			for row in result_list:
-				expected_type_dict[row['field_name']] = row['field_type']
-				expected_value_list = []
-				if row['expected_values'] is not None:
-					expected_value_list = list(expected_value_list)
-				expected_value_dict[row['field_name']] = expected_value_list
-		else:
-			reason = f"No attribute configured for form_id={self.form_id}"
-			return reason
+    def validate_form_data(self, submitted_dict):
+        """
+        This function validates if the submitted data is inline with
+        the template created by the developer. It checks the fields submitted
+        and the type of data received in the submitted value.
+        :return: Empty string if fields are valid, else returns the appropriate
+        error message.
+        """
+        template = {}
+        template['form_id'] = self.form_id
+        submitted_keys = submitted_dict.keys()
+        expected_type_dict = {}
+        expected_value_dict = {}
+        database_service = RDBDataTable(
+          "form_column_mapper",
+          connect_info=context.get_rdb_info(),
+          key_columns=["form_id", "field_name"])
+        result_list = database_service.find_by_template(
+          template, fields=['field_name', 'field_type', 'expected_values'])
+        if result_list is not None:
+            for row in result_list:
+                expected_type_dict[row['field_name']] = row['field_type']
+                expected_value_list = []
+                if row['expected_values'] is not None:
+                    expected_value_list = list(expected_value_list)
+                expected_value_dict[row['field_name']] = expected_value_list
+        else:
+            reason = f"No attribute configured for form_id={self.form_id}"
+            return reason
 
-		expected_key_list = expected_type_dict.keys()
-		missing_keys = list(set(expected_key_list) - set(submitted_keys))
-		if len(missing_keys) == 0:
-			for key, value in submitted_dict.items():
-				expected_type = DataValidator.get_value_type(expected_type_dict[key])
-				received_type = type(value)
-				# print("Expected type: " + str(expected_type))
-				# print("Received type: " + str(expected_type))
-				instance_state = isinstance(value, expected_type)
-				print("Instance: " + str(instance_state))
-				if not instance_state is True:
-					reason = f"For field={key}, expected ={expected_type.__name__}, received={received_type.__name__}"
-					return reason
-				
-				expected_values = expected_value_dict[key]
-				print("Expected values :" + str(expected_values))
-				if not len(expected_values)==0 and value not in expected_values:
-					reason = f"Expected={expected_values}, got ={value}"
-					return reason
-		else:
-			
-			reason = f"Missing fields={','.join(str(mkeys) for mkeys in missing_keys)} in the request. Please fill the form"
-			return reason
+        expected_key_list = expected_type_dict.keys()
+        missing_keys = list(set(expected_key_list) - set(submitted_keys))
+        if len(missing_keys) == 0:
+            for key, value in submitted_dict.items():
+                expected_type = DataValidator.get_value_type(
+                  expected_type_dict[key])
+                received_type = type(value)
+                # print("Expected type: " + str(expected_type))
+                # print("Received type: " + str(expected_type))
+                instance_state = isinstance(value, expected_type)
+                print("Instance: " + str(instance_state))
+                if instance_state is not True:
+                    reason = f"For field={key},\
+ expected ={expected_type.__name__}, received={received_type.__name__}"
+                    return reason
 
-		return ""
+                expected_values = expected_value_dict[key]
+                print("Expected values :" + str(expected_values))
+                if (not len(expected_values) == 0 and
+                   value not in expected_values):
+                    reason = f"Expected={expected_values}, got ={value}"
+                    return reason
+        else:
+            reason = f"Missing fields=\
+{','.join(str(mkeys) for mkeys in missing_keys)} in the request. \
+Please fill the form"
+            return reason
 
-	
-	def save_data(self, form_id, data):
-		"""
-		:param form_id: form_id for which the response has to be saved.
-		:param data: The data to be saved in the mongoDB. 
-		This functions saves the data in the mongoDB database.
-		:returns: response_id of the response submitted.
-		"""
-		mongodb_conn = context.get_mongo_db_info()
-		table_name = self.get_table_name(form_id)
-		response_id = RestUtils.id_generator(size=32)
-		data['response_id'] = response_id
-		mongo_client = MongoDBTable(table_name, connect_info=mongodb_conn, key_columns=['response_id'])
-		insert_id = mongo_client.insert(data)
-		current_app.logger.info(f"Data inserted with id ={insert_id}")
-		return response_id
+        return ""
 
+    def save_data(self, form_id, data):
+        """
+        :param form_id: form_id for which the response has to be saved.
+        :param data: The data to be saved in the mongoDB.
+        This functions saves the data in the mongoDB database.
+        :returns: response_id of the response submitted.
+        """
+        mongodb_conn = context.get_mongo_db_info()
+        table_name = self.get_table_name(form_id)
+        response_id = RestUtils.id_generator(size=32)
+        data['response_id'] = response_id
+        mongo_client = MongoDBTable(
+          table_name,
+          connect_info=mongodb_conn,
+          key_columns=['response_id'])
+        insert_id = mongo_client.insert(data)
+        current_app.logger.info(f"Data inserted with id ={insert_id}")
+        return response_id
 
-	def get_table_name(self, form_id):
-		"""
-		:param form_id: form_id for which the response has to be saved.
-		:returns: mongodb table name
-		"""
-		return str(form_id) + "_" + "response"
+    def get_table_name(self, form_id):
+        """
+        :param form_id: form_id for which the response has to be saved.
+        :returns: mongodb table name
+        """
+        return str(form_id) + "_" + "response"
